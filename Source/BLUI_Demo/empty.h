cbb4199b7b3a9791aaa6353f18d87751daabeee6
oivio <soulast25@hotmail.com>
2025-04-14 20:59:41 +0200

    Removed unused interface and made some cleaning

---
+++ Source/BLUI_Demo/empty.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BLUI_DEMO_API empty
+{
+public:
+	empty();
+	~empty();
+};
